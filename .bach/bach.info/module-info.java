181b06716ac9cd9dc46e8df8b6640b5f1da32316
Christian Stein <sormuras@gmail.com>
2021-04-12 12:04:48 +0200

    Format

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -3,11 +3,10 @@ import com.github.sormuras.bach.ProjectInfo.Options;
 import com.github.sormuras.bach.ProjectInfo.Tools;
 
 @ProjectInfo(
-  name = "app",
-  options = @Options(
-      tools = @Tools(limit = {"javac", "jar", "jlink"})
-  )
+    options = @Options(
+        tools = @Tools(limit = {"javac", "jar", "jlink"})
+    )
 )
 module bach.info {
- requires com.github.sormuras.bach;
+  requires com.github.sormuras.bach;
 }
